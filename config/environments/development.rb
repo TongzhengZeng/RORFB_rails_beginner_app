d80a13b38f9ddd1c4aa800299da1b39fa2ac9086
Tongzheng Zeng <tzeng000@citymail.cuny.edu>
2019-06-29 23:15:08 -0400

    comments and mail set up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    port:587,
+    domain:"example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["MAIL_USERNAME"],
+    password: ENV["MAIL_PASSWORD"]    
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
