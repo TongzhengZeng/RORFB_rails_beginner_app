69db326989d243168a0f326e6185145e5eaea9c3
Tongzheng Zeng <tzeng000@citymail.cuny.edu>
2019-06-30 01:08:50 -0400

    generate mailer, send comment emails, and seeding data, user data, Fake data

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form', '~> 4.1'
 gem 'devise', '~> 4.6', '>= 4.6.2'
 gem 'bootstrap-sass-extras'
+gem 'faker', '~> 1.9', '>= 1.9.4'
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(
+	email: 'ztzhltxy@126.com',
+	password: '123456',
+	password_confirmation: '123456',
+	first_name: 'Tongzheng',
+	last_name: 'Zeng',
+	username: 'ztzhlthjj'
+	)
+puts user.inspect
+
+
+user2 = User.create!(
+	email: 'ztzhltxy@hotmail.com',
+	password: '123456',
+	password_confirmation: '123456',
+	first_name: 'Tongzheng',
+	last_name: 'Zeng',
+	username: 'ztzhlth'
+	)
+puts user2.inspect
+
+
+50.times do
+	post = Post.create!(
+		title: Faker::Coffee.blend_name,
+		body: Faker::Coffee.notes,
+		user_id: rand(1..2)
+		)
+	puts post.inspect
+end
+
+500.times do
+	comment = Comment.create!(
+		body: Faker::Lorem.paragraph,
+		post_id: rand(1..50),
+		user_id: rand(1..2)
+		)
+	puts comment.inspect
+end
+
+
+# rake db:drop db:create db:migrate db:seed
